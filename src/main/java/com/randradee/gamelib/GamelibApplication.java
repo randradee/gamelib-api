b5d42fe608078f94c06cacd93829ede7f1a996bf
Renato Andrade <dev.renatopandrade@gmail.com>
2023-07-10 22:38:33 -0300

    initial commit

---
+++ src/main/java/com/randradee/gamelib/GamelibApplication.java
@@ -0,0 +1,13 @@
+package com.randradee.gamelib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/randradee/gamelib/GamelibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randradee.gamelib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
